26812c27dd83301ff43c1f098d26369f6cb89471
Eduardo Candaudap <candaudap@gmail.com>
2023-06-27 10:03:33 -0600

    Initial commit

---
+++ 1_vibora.py
@@ -0,0 +1,20 @@
+'''1. Escribe un script que imprima las primeras estrofas de la canción 
+de la víbora de la mar con el siguiente formato:
+
+A la víbora, víbora
+	De la mar, de la mar
+		Por aquí pueden pasar
+	Los de adelante corren mucho
+		Y los de atrás se quedarán
+
+A la víbora, víbora
+	De la mar, de la mar
+		Por aquí pueden pasar
+	Los de adelante corren mucho
+		Y los de atrás se quedarán
+		Tras, tras, tras'''
+
+print('A la víbora, víbora \n    De la mar, de la mar \n\tPor aquí pueden pasar \n    Los de adelante corren mucho \n\tY los de atrás se quedarán \n \
+      \n   \
+      A la víbora, víbora \n    De la mar, de la mar \n\tPor aquí pueden pasar \n    Los de adelante corren mucho \n\tY los de atrás se quedarán \n\tTras, tras, tras'
+      )
---
+++ 2_input_nombre.py
@@ -0,0 +1,19 @@
+'''2. Escribe un programa que solicite el nombre, el apellido y la edad del usuario 
+e imprima un saludo y su año de nacimiento. Tomar el año actual como constante (2023)
+
+Ejemplo.
+input
+Nombre: Alfredo
+Apellido: Altamirano
+Edad: 36
+
+output
+Hola Alfredo Altamirano, naciste en 1986
+'''
+
+nombre = input("Dame tu nombre: ")
+apellido = input("Escribe tu appelido paterno ")
+edad = input("Escribe tu edad: ")
+year = 2023 - int(edad)
+
+print(f'Hola {nombre} {apellido}, naciste en {year}')
